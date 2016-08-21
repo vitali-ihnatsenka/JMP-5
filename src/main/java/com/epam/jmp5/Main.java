3b5bdd4e0960c88e5e28c16debabf573e1a2122f
Vitali Ihnatsenka <vitt.ignat@gmail.com>
2016-08-21 08:56:30 +0300

    maven

---
+++ src/main/java/com/epam/jmp5/Main.java
@@ -0,0 +1,71 @@
+package com.epam.jmp5;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Main {
+
+    private final static Logger logger = LogManager.getLogger(Main.class);
+    private final static String DEAFAULT_CLASSES_PATH = "d://classes/";
+
+
+    public static void main(String[] args) throws IOException {
+        Path classesPath = Paths.get(DEAFAULT_CLASSES_PATH);
+        if(args.length > 0){
+            Paths.get(args[0]);
+        }
+        logger.info("Looking for classes in " + classesPath);
+
+        if(!Files.exists(classesPath)){
+            logger.error(classesPath + " path doesn't exist");
+            return;
+        }
+
+        List<Path> jarPaths = new ArrayList<Path>();
+
+        Files.walk(classesPath).forEach(path -> {
+            if(Files.isRegularFile(path) && path.toString().endsWith(".jar")){
+                jarPaths.add(path);
+            }
+        });
+
+        if(jarPaths.isEmpty()){
+            logger.error(classesPath + " doesn't contain any jar file");
+            return;
+        }
+
+        logger.info("Choose jar to load:");
+
+        IntStream.range(0, jarPaths.size()).forEach(indx -> {
+            logger.info(indx + " - " + jarPaths.get(indx));
+        });
+
+        int userChoice = getIntInput(jarPaths.size() - 1);
+
+        Path chosenPath =jarPaths.get(userChoice);
+
+        logger.info("You chosen " + chosenPath);
+    }
+
+    private static int getIntInput(int maxValue){
+        try {
+            int input = Integer.parseInt(System.console().readLine());
+            if(input < 0 || input > maxValue){
+                logger.info("Choose number from 0 to " + maxValue);
+                return getIntInput(maxValue);
+            }
+            return input;
+        }catch(NumberFormatException e){
+            logger.info("Numbers only.");
+            return getIntInput(maxValue);
+        }
+    }
+}
